0b68cc1ec04b1cc4ae89174a0ad0e2e885f43e4e
avf99 <90771483+avf99@users.noreply.github.com>
2022-12-18 14:21:10 +0330

    Add files via upload

--- code.js
+++ code.js
@@ -15,3 +15,18 @@ if(getChromeVersion() <= 70) {
     root.style.setProperty('--crab-size', crabSize + "px");
     root.style.setProperty('--sun-diameter', sunDiameter + "px");
 }
+
+function blowOut() {
+    var smokes = document.getElementsByClassName("smoke");
+    var flame = document.getElementsByClassName("flame");
+    
+    for (let i = 0; i < flame.length; i++) {
+        flame[i].style.opacity = 0;
+    }
+    
+    for (let i = 0; i < smokes.length; i++) {
+        smokes[i].style.animation = "flame-off 3s linear forwards";
+        smokes[i].style.animationDelay = i%5 * 0.5 + "s";
+    }
+    
+}
